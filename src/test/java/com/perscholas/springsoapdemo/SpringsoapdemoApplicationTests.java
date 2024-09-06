22554d76f6ec6f162b3572a398acea3134b244af
Mscourtneybryant <87495361+Mscourtneybryant@users.noreply.github.com>
2024-09-06 11:10:32 -0500

    start task

---
+++ src/test/java/com/perscholas/springsoapdemo/SpringsoapdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.springsoapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsoapdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
